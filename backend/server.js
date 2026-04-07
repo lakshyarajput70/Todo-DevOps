c2ffd2901ee445827c4d91d96d3fc4d25d2a822a
lakshyawork70 <lakshyawork70@gmail.com>
2026-04-07 08:41:44 +0530

    render/vercel adding cors origin

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,23 @@ const mongoose=require("mongoose");
 const cors=require("cors");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+//Allow multiple origins
+const allowedOrigins = ['http://localhost:5173','https://todo-dev-ops.vercel.app/']
+
 const app=express();
-app.use(cors());
+app.use(cors({origin:allowedOrigins , credentials :true}))
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Mongo connected"));
+
 
 const Todo = mongoose.model("Todo",{
  text:String,
